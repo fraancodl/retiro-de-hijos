cee48d2921ffcdc32ffe1ca7cddba3a1ccb5ee37
fraancodl <franco12103485@gmail.com>
2022-11-25 11:46:51 -0300

    a

--- src/ScreenLogIn/App.js
+++ src/ScreenLogIn/App.js
@@ -1,53 +1,27 @@
 import './App.css';
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import { render } from '@testing-library/react';
 
 function App() {
-
-  const [data, setData] = useState();
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  useEffect(() => {
-
-  const getData = async () => {
-    let id = getCookie("LoggedId")
-
-    let token = "token=" + id;
-    console.log(token);
-
-    const response = await fetch("http://localhost:5001/estacionamiento/ordenar?LoggedId="+id, {
-      method: "GET",
-      mode: "cors",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // body: JSON.stringify(id)
-    })
-      const json = await response.json()
-      const data = json
-      setData(data)
-    return data
-  }
-
-    let lol = getData();
-    console.log(lol);
-  }, []);
-
-
+  const estudiantes = [
+    {id: 1, name: 'Juan', curso: '5c'},
+    {id: 2, name: 'Martin', curso: '5c'},
+    {id: 3, name: 'Fede', curso: '5c'},
+  ];
+    //   const [hijos, setHijos] = useState(" ")
+    // const response = fetch("http://localhost:5001/estacionamiento/ordenar", {
+    //   method: "GET",
+    //   mode: "cors",
+    //   credentials: "include",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   }
+    // }).then( (res) => res.json()).then((json) => {
+    //   this.setState({items: json})
+    //   setHijos(json)
+    //   console.log(hijos)
+    // })
 
   return (
     <div>
@@ -57,12 +31,17 @@ function App() {
       <div className="contenedor">
         <h1>¿A quien quieres retirar?</h1>
         <h2>indica con un tic a quien queres retirar. Aquellos espacios que quedan en blanco no estaran disponibles para el retiro</h2>
-        {<p>{data}</p>}
         <form>
-          <input type="checkbox" />
-          <input type="button" className="logearse" value="Solicitar dársena" />
-        </form>
+      {
+        estudiantes.map((estudiante) => {
+       return <label><input type="checkbox" /> {estudiante.name} {estudiante.curso}
+       
+       </label>
+        }) 
+      }
+      </form>
       </div>
+      
     </div>
   );
 }
